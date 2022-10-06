2a51ec503f942bf24280c63931cd32b75dd8b9c2
Vice Sallés <vicenc.salles@gmail.com>
2022-10-06 17:03:48 +0200

    Introduïdes les paraules. Fallar no descarta paraula

--- js/app.js
+++ js/app.js
@@ -2,32 +2,30 @@
 // -----------------------------------------------------------------------------
 
 var words = [
-	new Word(0, "A", "Amb la lletra A:", " Bèstia, fera", "Animal"),
-	new Word(1, "B", "Amb la lletra B:", " Lorem ipsum dolor sit amet", "Bollo"),
-	new Word(2, "C", "Amb la lletra C:", " cae desde cierta altura a causa de un brusco desnivel en su cauce, especialmente en un rio", "Cascada"),
-	new Word(3, "D", "Amb la lletra D:", " rta, ancha y puntiaguda, parecida a la espada pero de menor tamaño", "Daga"),
-	new Word(4, "E", "Amb la lletra E:", " be varias vueltas alrededor de un punto, alejándose cada vez más de él", "Espiral"),
-	new Word(5, "F", "Amb la lletra F:", " o podrido por la acción de diversos factores y determinados microorganismos", "Putrefacto"),
-	new Word(6, "G", "Amb la lletra G:", " era ruda, tosca o grosera", "Garrulo"),
-	new Word(7, "H", "Amb la lletra H:", " s grueso y de poca altura", "Rechoncho"),
-	new Word(8, "I", "Amb la lletra I:", "  existente entre dos astros, o que tiene relación con él", "Interestelar"),
-	new Word(9, "J", "Amb la lletra J:", " 5 cm de largo, carnoso y de punta redonda, que se usa para condimentar ciertos guisos", "Jalapeño"),
-	new Word(10, "K", "Amb la lletra K:", " Pseudònim de l'oncle Josep", "Koba"),
-	new Word(11, "L", "Amb la lletra L:", " Lorem ipsum dolor sit amet ", "Lorem"),
-	new Word(12, "M", "Amb la lletra M:", " Persona que sufre o muere por defender su religión o sus ideales. ", "Martir"),
-	new Word(13, "N", "Amb la lletra N:", " Tubo fluorescente que produce una luz brillante.", "Neon"),
-	new Word(14, "NY", "Amb la lletra NY:", " Dar a una cosa un color distinto del que tiene.", "Teñir"),
-	new Word(15, "O", "Amb la lletra O:", " Que conoce todas las cosas reales y posibles.", "Omnisciente"),
-	new Word(16, "P", "Amb la lletra P:", " Calzado de lona, con suela de esparto, cáñamo o goma, que se sujeta al pie por presión o con unas cintas que se atan al tobillo.", "Alpargata"),
-	new Word(17, "Q", "Amb la lletra Q:", " Que se puede romper fácilmente.", "Quebradizo"),
-	new Word(18, "R", "Amb la lletra R:", " Operación quirúrgica para restaurar la nariz.", "Rinoplastia"),
-	new Word(19, "S", "Amb la lletra S:", " Falta de cuidado en la forma de vestir y en el aseo personal.", "Desaliño"),
-	new Word(20, "T", "Amb la lletra T:", " Persona alocada, bulliciosa y molesta.", "Tabardillo"),
-	new Word(21, "U", "Amb la lletra U:", " Persona que rehúye el trato de otras personas y rechaza las atenciones y muestras de cariño.", "Huraño"),
-	new Word(22, "V", "Amb la lletra V:", " Suc diví.", "Vi"),
-	new Word(23, "X", "Amb la lletra X:", " Punto culminante o de mayor satisfacción de la excitación sexual en las zonas erógenas o sexuales.", "Climax"),
-	new Word(24, "Y", "Amb la lletra Y:", " Toro castrado, que se utiliza como animal de tiro y del cual se aprovecha su carne.", "Buey"),
-	new Word(25, "Z", "Amb la lletra Z:", " Que es tonto o tiene poca rapidez mental.", "Pazguato")
+	new Word(0, "A", "Amb la lletra A:", " On et vas fotre un bitxo tant coent que et sortia cera de les orelles, tot i els avisos previs del cambrer.", "Alacant"),
+	new Word(1, "B", "Amb la lletra B:", " Enemic total gastronòmic contra el que lluites aferrissadament fent proselitisme de la teva causa a les nostres filles.", "Bledes"),
+	new Word(2, "C", "Amb la lletra C:", " Illes de les que vas ser deportat per un assumpte fosc que feia pudor.", "Canàries"),
+	new Word(3, "D", "Amb la lletra D:", " El sobrenom que més t'escau.", "Dat pel cul"),
+	new Word(4, "E", "Amb la lletra E:", " Disfressa amb la que et vas presentar al carnaval de Solsona, on en comptes de disfresses s’utilitzen bates de colors.", "Exèrcit Roig"),
+	new Word(5, "F", "Amb la lletra F:", " Quina muntanya berguedana vam pujar 5 persones dues vegades amb un dorsal a l’esquena el dia de cap d’any?", "Figuerassa"),
+	new Word(6, "G", "Amb la lletra G:", " Lloc on vas estar apunt d'ingressar per fer un 'grafitti' (tipus Diana i no de Gal-les) a la façana d'un conegut supermercat de Berga.", "Garjola"),
+	new Word(7, "H", "Amb la lletra H:", " Nom del filòsof que va dir (més o menys) el següent: No podem rebre dues vegades la mateixa sodomia per part de l'Òscar. Quan hi torna, ni la seva tranca ni el nostre cul són els mateixos.", "Heràclit"),
+	new Word(8, "I", "Amb la lletra I:", "  Organització internacionalista que, gràcies a tu, és el gra al cul de la CUP.", "Ítaca"),
+	new Word(9, "J", "Amb la lletra J:", " Autor de la cançó exitosament coreografiada per cap d'any, a casa vostra, i que de manera infame vas fer pública. Pista: \"... és la teva forma de ser...\"", "Josep Maria Cantimplora"),
+	new Word(10, "K", "Amb la lletra K:", " Nom d'una de les poques repúbliques exsoviètiques que no has visitat.", "KAZAKHSTAN"),
+	new Word(11, "L", "Amb la lletra L:", " Estat en què et van dur a l'hospital després d'una nit de 'juerga' amb els amics quan eres un preadolescent.", "Lamentable"),
+	new Word(12, "LL", "Amb la lletra LL:", " El teu sobrenom quan pedales per àfrica.", "El Llantes"),
+	new Word(13, "M", "Amb la lletra M:", " El nostre primer viatge..... i on et vas banyar al mar amb les claus del cotxe a la butxaca del banyador.....qui ho havia de dir que llavors no el podríem obrir!!!!", "Mallorca"),
+	new Word(14, "NY", "Amb la lletra NY:", " El sobrenom amb el que et coneixen al Xino Xano.", "Nyanyo"),
+	new Word(15, "O", "Amb la lletra O:", " La casa que vem marxar amb el cotxe en marxa quan uns “pocs amics”ens perseguien", "Okupa"),
+	new Word(16, "P", "Amb la lletra P:", " On vem provar de dormir fent-nos passar per peregrins del camí de sant Jaume.", "Pamplona"),
+	new Word(17, "Q", "Amb la lletra Q:", " La primera via llarga que vas haver d'arrossegar l'Eva.", "Queraltina"),
+	new Word(18, "R", "Amb la lletra R:", " L’amic que vem fer quan vem anar a buscar samarretes per vendre i pagar la multa dels “graffitis”.", "Rota"),
+	new Word(19, "S", "Amb la lletra S:", " nom de l'entrentrenament infernal fet a Pedret i del qual et vas escollonar, que constava en 10 repeticions x400 mts.", "Sèries"),
+	new Word(20, "T", "Amb la lletra T:", " Sobrenom polític.", "Trotskar."),
+	new Word(21, "V", "Amb la lletra V:", " Líquid diví que no pot ser tot degustat en una sola nit.", "Vi"),
+	new Word(22, "X", "Amb la lletra X:", " Nom de la via, on en el 3er llarg et va caure un peu d gat…..", "Xibalbà"),
+	new Word(23, "Z", "Amb la lletra Z:", " Personeta a la que Li has ficat mes mandarines dintre la roba, i a la que vash traumar per que es pensaba que tenía bonys per tot el cos...Com t'agrada Fer bromas a tothom...", "Zulema")
 ];
 
 // Functions
@@ -47,22 +45,24 @@ function showDefinition(pos) {
 	$("#js--definition").html(words[pos].definition);
 }
 
-var remainingWords = 25;
+var remainingWords = 24;
 
 function checkAnswer(pos) {
 	var userAnswer = $("#js--user-answer").val().toLowerCase();
+	
 	if (userAnswer == words[pos].word.toLowerCase()) {
 		words[pos].correct = true;
+		$(".circle .item").eq(words[pos].idNumber).removeClass("item--failure");
 		$(".circle .item").eq(words[pos].idNumber).addClass("item--success");
-
+		remainingWords--;
+		$("js--score").html(remainingWords);
+		count++
 	} else {
 		words[pos].correct = false;
 		$(".circle .item").eq(words[pos].idNumber).addClass("item--failure");
-	}
-	remainingWords--;
-	$("js--score").html(remainingWords);
+		pasapalabra(pos);
+	}	
 
-	return count++;
 }
 
 function pasapalabra(pos) {
@@ -72,7 +72,7 @@ function pasapalabra(pos) {
 }
 
 function continuePlaying() {
-	if (count != 25) {
+	if (count != 24) {
 		$("#js--user-answer").val("");
 		showDefinition(count);
 	} else {
@@ -106,6 +106,14 @@ function endGame() {
 	$("#js--close").addClass("hidden")
 }
 
+function gameOver() {
+	$("#js--question-controls").addClass("hidden");
+	$("#js--pa-controls").removeClass("hidden");
+	$("#js--end-title").html("Torna-ho a provar");
+	$("#js--end-subtitle").html(showUserScore());
+	$("#js--close").addClass("hidden")
+}
+
 function showUserScore() {
 	var counter = 0;
 	for (i = 0; i < words.length; i++) {
@@ -127,8 +135,10 @@ $("#js--new-game").click(function() {
 	$("#js--ng-controls").addClass("hidden");
 	$("#js--question-controls").removeClass("hidden");
 	$("#js--close").removeClass("hidden");
+	$("js--user-answer").focus();
 	showDefinition(count);
 	countdown();
+	
 });
 
 // Send the answer
@@ -152,15 +162,6 @@ $("#js--pasapalabra").click(function() {
 	continuePlaying();
 });
 
-// Key bindings for skip the word
-$("#js--question-controls").keypress(function(event) {
-	var keycode = (event.keyCode ? event.keyCode : event.which);
-	if (keycode == "32") {
-		pasapalabra(count);
-		continuePlaying();
-	}
-});
-
 // Play again
 $("#js--pa").click(function() {
 	location.reload()
@@ -168,5 +169,5 @@ $("#js--pa").click(function() {
 
 // End the game
 $("#js--close").click(function() {
-	endGame();
+	gameOver();
 });
